c0c891a77da5ce27881f134efa3a527cb938e82f
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-04-12 16:08:28 +0200

    Finished 8.5

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Podaj wartość a:'),
+	h = prompt('Podaj wartość h:'),
+	triangleArea = a * h / 2;
+
+console.log("Triangle area with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
